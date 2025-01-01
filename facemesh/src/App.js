b9446a9a3d76c4516a13dac198b900ccae652e23
Lovish Bir <53646879+lovishbir@users.noreply.github.com>
2025-01-01 12:07:15 +0530

    updates

--- facemesh/src/App.js
+++ facemesh/src/App.js
@@ -19,10 +19,45 @@ import * as tf from '@tensorflow/tfjs';
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
 
+function App(){
+  // Setup references
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null) 
+}
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
     </div>
   );
 }
