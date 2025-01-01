bf7c819e2cbb705255e198de56c12938c6d69e03
Lovish Bir <53646879+lovishbir@users.noreply.github.com>
2025-01-01 12:03:58 +0530

    added webcam

--- facemesh/src/App.js
+++ facemesh/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// 1. Install dependencies 
+// 2. Import dependencies 
+// 3. Setup webcam and canvas 
+// 4. Define references to those 
+// 5. Load posenet 
+// 6. Detect function 
+// 7. Drawing utilities
+// 8. Load triangulation
+// 9. Setup triangular path
+// 10. setup point drawing
+// 11. add drawMesh to detect function
+
+
+
+import React,{useRef} from 'react'
+//import logo from './logo.svg';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
